827c335534d46945fa5ddf6019059fbfb3a5b5fb
Luxana1 <96484101+Luxana1@users.noreply.github.com>
2024-10-10 15:58:52 +0100

    images added
    Added some images to attach them to the button

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
 const myButton = document.getElementById("btn");
 const myParagraph = document.getElementById("my-paragraph");
+const characterImage = document.getElementById("character-img")
 
 let clickCounter = 0;
 
 function clickFunction() {
+    console.log("the suffering button have been clicked to spread more suffering")
     clickCounter = clickCounter + 1;
+    characterImage.src = "assets/puppies.jpg";
     document.getElementById("my-paragraph").innerHTML = "Suffering!!!" + clickCounter + "times";
 }
 btn.addEventListener("click", clickFunction);
