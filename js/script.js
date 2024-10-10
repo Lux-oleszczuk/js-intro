a19639e10fa04fd6fd40cd2d3a2663bc2e219e06
Luxana1 <96484101+Luxana1@users.noreply.github.com>
2024-10-10 16:23:00 +0100

    comments
    Added a bunch of comments.

--- js/script.js
+++ js/script.js
@@ -2,18 +2,23 @@ const imageOne = "assets/kitty.jpg";
 const imageTwo = "assets/puppies.jpg";
 const imageThree = "assets/weirdkitty.jpg";
 
+//
 const myButton = document.getElementById("btn");
 const myParagraph = document.getElementById("my-paragraph");
 const characterImage = document.getElementById("character-img")
-
-let clickCounter = 0;
+//store which character is currently being displayed
 let characterCounter = 1;
 
-function clickFunction() {
-    console.log("the suffering button have been clicked to spread more suffering")
-    clickCounter = clickCounter + 1;
-    characterCounter = characterCounter + 1;
+/**
+ * Update character counter and display next one
+ * set counter back to 1 of 3 was surpassed
+ * @returns  when character is chosen
+ */
 
+function clickFunction() {
+//update character counter
+    characterCounter = characterCounter + 1;
+//set up the character counter back to 1
     if(characterCounter > 3) {
         characterCounter = 1;
     }
@@ -31,6 +36,5 @@ function clickFunction() {
         characterImage.src = imageThree;
     } return;
 
-    document.getElementById("my-paragraph").innerHTML = "Suffering!!!" + clickCounter + "times";
 }
 btn.addEventListener("click", clickFunction);
