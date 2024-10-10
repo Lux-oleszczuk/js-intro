de1ed3e307cf9ade4c68c01460a61ffd7a66d0a0
Luxana1 <96484101+Luxana1@users.noreply.github.com>
2024-10-10 16:16:52 +0100

    returns
    add returns to avoid necessary errors

--- js/script.js
+++ js/script.js
@@ -1,13 +1,36 @@
+const imageOne = "assets/kitty.jpg";
+const imageTwo = "assets/puppies.jpg";
+const imageThree = "assets/weirdkitty.jpg";
+
 const myButton = document.getElementById("btn");
 const myParagraph = document.getElementById("my-paragraph");
 const characterImage = document.getElementById("character-img")
 
 let clickCounter = 0;
+let characterCounter = 1;
 
 function clickFunction() {
     console.log("the suffering button have been clicked to spread more suffering")
     clickCounter = clickCounter + 1;
-    characterImage.src = "assets/puppies.jpg";
+    characterCounter = characterCounter + 1;
+
+    if(characterCounter > 3) {
+        characterCounter = 1;
+    }
+
+    myParagraph.innerHTML = "I am a character" + characterCounter;
+
+
+    if(characterCounter === 1) {
+        characterImage.src = imageOne;
+    } return;
+    if(characterCounter === 2) {
+        characterImage.src = imageTwo;
+    } return;
+    if(characterCounter === 3) {
+        characterImage.src = imageThree;
+    } return;
+
     document.getElementById("my-paragraph").innerHTML = "Suffering!!!" + clickCounter + "times";
 }
 btn.addEventListener("click", clickFunction);
