c938e137500d8f7ee27c1105e6a61c299feb4f34
Lux-oleszczuk <luxana.oleszczuk@student.norwichuni.ac.uk>
2024-10-15 10:16:44 +0100

    alt text
    added alt text to images

--- js/script.js
+++ js/script.js
@@ -1,13 +1,22 @@
+//defne cources for each image
 const imageOne = "assets/kitty.jpg";
 const imageTwo = "assets/puppies.jpg";
 const imageThree = "assets/weirdkitty.jpg";
 
-//
+//store which character is currently being displayed
 const myButton = document.getElementById("btn");
 const myParagraph = document.getElementById("my-paragraph");
 const characterImage = document.getElementById("character-img")
-//store which character is currently being displayed
+
+//define alternative text
+const altText1 = "A weird kitty"
+const altText2 = "Cyriak's Yorkshire puppies that have furry snake tails instead of a common puppies' body"
+const altText3 = "a kitty with a big human eyes instead of kotten's eyes"
+
 let characterCounter = 1;
+
+// const images = ["assets/kitty.jpg", "assets/puppies.jpg","assets/weirdkitty.jpg" ]
+
 
 /**
  * Update character counter and display next one
@@ -22,19 +31,25 @@ function clickFunction() {
     if(characterCounter > 3) {
         characterCounter = 1;
     }
-
-    myParagraph.innerHTML = "I am a character" + characterCounter;
+//update paragraph text with a new counter
+myParagraph.innerHTML = "I am a character" + characterCounter;
 
 
     if(characterCounter === 1) {
         characterImage.src = imageOne;
-    } return;
+        characterImage.src = altText1;
+        return;
+    }
     if(characterCounter === 2) {
         characterImage.src = imageTwo;
-    } return;
+        characterImage.src = altText2;
+        return;
+    }
     if(characterCounter === 3) {
         characterImage.src = imageThree;
-    } return;
+        characterImage.src = altText3;
+        return;
+    }
 
 }
 btn.addEventListener("click", clickFunction);
